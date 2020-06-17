ff4e26f4930fe3c3b7968ecc4f082925cd0692f2
abhishek-shashank-au6 <abhishek.shashank02@gmail.com>
2020-06-17 20:49:20 +0530

    flake8 modifications

--- organiser.py
+++ organiser.py
@@ -4,19 +4,19 @@ import argparse
 import os
 import shutil
 import datetime
-from pathlib import Path
 
 
 # Command Line Interfacing in the Main() Function
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--d', type=str, default=os.getcwd(),
-                        help='Absolute path of the directory')
+                        help='Absolute path of the directory to be Organised')
     parser.add_argument('--o', type=str, default='ext',
                         choices=['ext', 'size', 'date'],
                         help='What Operation? (ext, size or date)')
     args = parser.parse_args()
     organise_folder(args)
+
 
 # Dictionary of Directories Based on Extensions
 DIRECTORIES = {
@@ -51,109 +51,114 @@ FILE_FORMATS = {file_format: directory
 
 def organise_folder(args):
     cwd = args.d    # Directory Path Given through the CLI
-    if args.o == 'ext':  # Organising Option Given through the CLI
-        for entry in os.scandir(cwd):
-            # Recursively Iterating thorugh all the files in the Directory
-            if not entry.is_dir():
-                file_path = os.path.abspath(entry)
-                if os.path.basename(file_path) == "organiser.py":
-                    # Skipping the Organiser File
-                    continue
-                file_format = os.path.splitext(file_path)[1].lower()
-                if file_format in FILE_FORMATS:
+    if os.path.isdir(cwd) is not False:
+        if args.o == 'ext':  # Organising Option Given through the CLI
+            for entry in os.scandir(cwd):
+                # Recursively Iterating thorugh all the files in the Directory
+                if not entry.is_dir():
+                    file_path = os.path.abspath(entry)
+                    if os.path.basename(file_path) == "organiser.py":
+                        # Skipping the Organiser File
+                        continue
+                    file_format = os.path.splitext(file_path)[1].lower()
+                    if file_format in FILE_FORMATS:
+                        directory_path = os.path.join(cwd, "Organised")
+                        if not os.path.exists(directory_path):
+                            # Creating the 'Organised' Folder in the Directory
+                            os.mkdir(directory_path)
+                        dest_folder = os.path.join(directory_path,
+                                                   FILE_FORMATS[file_format])
+                        if not os.path.exists(dest_folder):
+                            # Creating Directories Based On Extensions
+                            os.mkdir(dest_folder)
+                        # Copying the Current File Being Iterated over
+                        shutil.copy2(file_path, dest_folder)
+                        dest = os.path.join(dest_folder, entry)
+                        if os.path.exists(dest):
+                            # Deleting the File from the Original Directory
+                            os.remove(file_path)
+                    else:
+                        # If Extension not found in the Dictionary
+                        directory_path = os.path.join(cwd, "Other")
+                        if not os.path.exists(directory_path):
+                            os.mkdir(directory_path)
+                        shutil.copy2(file_path, directory_path)
+                        dest = os.path.join(directory_path, entry)
+                        if os.path.exists(dest):
+                            os.remove(file_path)
+
+        if args.o == 'size':
+            for entry in os.scandir(cwd):
+                if not entry.is_dir():
+                    file_path = os.path.abspath(entry)
+                    if os.path.basename(file_path) == "organiser.py":
+                        continue
+                    # Getting the File Size
+                    file_size = os.stat(file_path).st_size/1000
+                    if file_size <= 10:
+                        # Small Files
+                        directory_path = os.path.join(cwd, "Organised")
+                        if not os.path.exists(directory_path):
+                            os.mkdir(directory_path)
+                        dest_folder = os.path.join(directory_path, "Small")
+                        if not os.path.exists(dest_folder):
+                            os.mkdir(dest_folder)
+                        shutil.copy2(file_path, dest_folder)
+                        dest = os.path.join(dest_folder, entry)
+                        if os.path.exists(dest):
+                            os.remove(file_path)
+                    elif file_size > 10 and file_size <= 10000:
+                        # Medium Sized Files
+                        directory_path = os.path.join(cwd, "Organised")
+                        if not os.path.exists(directory_path):
+                            os.mkdir(directory_path)
+                        dest_folder = os.path.join(directory_path, "Medium")
+                        if not os.path.exists(dest_folder):
+                            os.mkdir(dest_folder)
+                        shutil.copy2(file_path, dest_folder)
+                        dest = os.path.join(dest_folder, entry)
+                        if os.path.exists(dest):
+                            os.remove(file_path)
+                    else:
+                        # Large Files
+                        directory_path = os.path.join(cwd, "Organised")
+                        if not os.path.exists(directory_path):
+                            os.mkdir(directory_path)
+                        dest_folder = os.path.join(directory_path, "Large")
+                        if not os.path.exists(dest_folder):
+                            os.mkdir(dest_folder)
+                        shutil.copy2(file_path, dest_folder)
+                        dest = os.path.join(dest_folder, entry)
+                        if os.path.exists(dest):
+                            os.remove(file_path)
+
+        if args.o == 'date':
+            for entry in os.scandir(cwd):
+                if not entry.is_dir():
+                    file_path = os.path.abspath(entry)
+                    if os.path.basename(file_path) == "organiser.py":
+                        continue
+                    # Getting the Latest Modified Date of the Current File
+                    file_mtime = os.stat(file_path).st_mtime
+                    file_timestamp = datetime.datetime.fromtimestamp(
+                        file_mtime
+                        ).strftime('%Y-%m-%d')
                     directory_path = os.path.join(cwd, "Organised")
                     if not os.path.exists(directory_path):
-                        # Creating the 'Organised' Folder inside the Directory
                         os.mkdir(directory_path)
-                    dest_folder = os.path.join(directory_path,
-                                               FILE_FORMATS[file_format])
+                    dest_folder = os.path.join(directory_path, file_timestamp)
                     if not os.path.exists(dest_folder):
-                        # Creating Different Directories Based On Extensions
                         os.mkdir(dest_folder)
-                    # Copying the Current File Being Iterated over
                     shutil.copy2(file_path, dest_folder)
                     dest = os.path.join(dest_folder, entry)
-                    if os.path.exists(dest):
-                        # Deleting the File from the Original Directory
-                        os.remove(file_path)
-                else:
-                    # If Extension not found in the Dictionary
-                    directory_path = os.path.join(cwd, "Other")
-                    if not os.path.exists(directory_path):
-                        os.mkdir(directory_path)
-                    shutil.copy2(file_path, directory_path)
-                    dest = os.path.join(directory_path, entry)
                     if os.path.exists(dest):
                         os.remove(file_path)
 
-    if args.o == 'size':
-        for entry in os.scandir(cwd):
-            if not entry.is_dir():
-                file_path = os.path.abspath(entry)
-                if os.path.basename(file_path) == "organiser.py":
-                    continue
-                # Getting the File Size
-                file_size = os.stat(file_path).st_size/1000
-                if file_size <= 10:
-                    # Small Files
-                    directory_path = os.path.join(cwd, "Organised")
-                    if not os.path.exists(directory_path):
-                        os.mkdir(directory_path)
-                    dest_folder = os.path.join(directory_path, "Small")
-                    if not os.path.exists(dest_folder):
-                        os.mkdir(dest_folder)
-                    shutil.copy2(file_path, dest_folder)
-                    dest = os.path.join(dest_folder, entry)
-                    if os.path.exists(dest):
-                        os.remove(file_path)
-                elif file_size > 10 and file_size <= 10000:
-                    # Medium Sized Files
-                    directory_path = os.path.join(cwd, "Organised")
-                    if not os.path.exists(directory_path):
-                        os.mkdir(directory_path)
-                    dest_folder = os.path.join(directory_path, "Medium")
-                    if not os.path.exists(dest_folder):
-                        os.mkdir(dest_folder)
-                    shutil.copy2(file_path, dest_folder)
-                    dest = os.path.join(dest_folder, entry)
-                    if os.path.exists(dest):
-                        os.remove(file_path)
-                else:
-                    # Large Files
-                    directory_path = os.path.join(cwd, "Organised")
-                    if not os.path.exists(directory_path):
-                        os.mkdir(directory_path)
-                    dest_folder = os.path.join(directory_path, "Large")
-                    if not os.path.exists(dest_folder):
-                        os.mkdir(dest_folder)
-                    shutil.copy2(file_path, dest_folder)
-                    dest = os.path.join(dest_folder, entry)
-                    if os.path.exists(dest):
-                        os.remove(file_path)
+        # Returning the Path of the Organised Folder
+        sys.stdout.write("Organised Path => " + os.path.join(cwd, "Organised"))
 
-    if args.o == 'date':
-        for entry in os.scandir(cwd):
-            if not entry.is_dir():
-                file_path = os.path.abspath(entry)
-                if os.path.basename(file_path) == "organiser.py":
-                    continue
-                # Getting the Latest Modified Date of the Current File
-                file_mtime = os.stat(file_path).st_mtime
-                file_timestamp = datetime.datetime.fromtimestamp(
-                    file_mtime
-                    ).strftime('%Y-%m-%d')
-                directory_path = os.path.join(cwd, "Organised")
-                if not os.path.exists(directory_path):
-                    os.mkdir(directory_path)
-                dest_folder = os.path.join(directory_path, file_timestamp)
-                if not os.path.exists(dest_folder):
-                    os.mkdir(dest_folder)
-                shutil.copy2(file_path, dest_folder)
-                dest = os.path.join(dest_folder, entry)
-                if os.path.exists(dest):
-                    os.remove(file_path)
-    # Returning the Path of the Organised Folder
-    sys.stdout.write(os.path.join(cwd, "Organised"))
+    else:
+        sys.stdout.write("Invalid Path! Please Enter a Valid Path")
 
     # Removing any Empty Directories
     for dir in os.scandir():
@@ -162,6 +167,7 @@ def organise_folder(args):
                 os.rmdir(dir)
         except BaseException:
             pass
+
 
 if __name__ == "__main__":
     main()
